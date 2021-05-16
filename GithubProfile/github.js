14bb1ea08f9002b84f22902fe7c20e05ff660e5a
Ayush <ayushbante6@gmail.com>
2021-05-16 18:05:01 +0530

    github profile project

---
+++ GithubProfile/github.js
@@ -0,0 +1,23 @@
+
+let profile_name = document.getElementById("name");
+let bio = document.getElementById("bio");
+let followers = document.getElementById("followers");
+let following = document.getElementById("following");
+let repos = document.getElementById("repos");
+let profile_image = document.getElementById("profile_image");
+
+function getGithubProfile(){
+    const username = document.getElementById('username').value;
+    fetch(`http://api.github.com/users/${username}`)
+  .then(response =>response.json())
+  .then(data => {
+    profile_name.innerHTML = data.name;
+    bio.innerHTML = data.bio;
+    profile_image.src = data.avatar_url;
+    followers.innerHTML = data.followers;
+    following.innerHTML = data.following;
+    repos.innerHTML = data.public_repos;
+  });
+}
+
+getGithubProfile();
