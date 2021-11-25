3c6699fc4ff01bf7d3f02f4d5f398cb6f02faf4a
Ayush <ayushbante6@gmail.com>
2021-11-25 21:52:31 +0530

    updated url

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const projects = [
     {
         name:'Movie App',
-        img_path:'images/movie_app.png',
+        img_path:'images/movie_app.PNG',
         overview:'',
         url:'movie-app'
     },
     {
         name:'Poke Dex',
-        img_path:'images/pokedex.png',
+        img_path:'images/pokedex.PNG',
         overview:'Simple Application build with <a href="https://www.accuweather.com">Accuweather</a> API.Used Tailwindcss for css.',
         url:'PokeDex'
     },
@@ -19,97 +19,97 @@ const projects = [
     },
     {
         name:'Music Player',
-        img_path:'images/music_player.png',
+        img_path:'images/music_player.PNG',
         overview:'Simple Application build with <a href="https://www.accuweather.com">Accuweather</a> API.Used Tailwindcss for css.',
         url:'MusicPlayer'
     },
     {
         name:'Mobile Nav Design',
-        img_path:'images/mobile_nav.png',
+        img_path:'images/mobile_nav.PNG',
         overview:'Simple Application build with <a href="https://www.accuweather.com">Accuweather</a> API.Used Tailwindcss for css.',
         url:'mobile-nav'
     },
     {
         name:'Github Profile App',
-        img_path:'images/github_profile.png',
+        img_path:'images/github_profile.PNG',
         overview:'Simple Application build with <a href="https://www.accuweather.com">Accuweather</a> API.Used Tailwindcss for css.',
         url:'GithubProfile'
     },
     {
         name:'Weather App',
-        img_path:'images/github_profile.png',
+        img_path:'images/github_profile.PNG',
         overview:'Simple Application build with <a href="https://www.accuweather.com">Accuweather</a> API.Used Tailwindcss for css.',
         url:'WeatherApp'
     },
     {
         name:'HoverBoard App',
-        img_path:'images/hover_board.png',
+        img_path:'images/hover_board.PNG',
         overview:'Simple Application build with <a href="https://www.accuweather.com">Accuweather</a> API.Used Tailwindcss for css.',
         url:'HoverBoard'
     },
     {
         name:'Calculator',
-        img_path:'images/calculator.png',
+        img_path:'images/calculator.PNG',
         overview:'Simple Application build with <a href="https://www.accuweather.com">Accuweather</a> API.Used Tailwindcss for css.',
         url:'Calculator'
     },
     {
         name:'Blink Effect',
-        img_path:'images/blink.png',
+        img_path:'images/blink.PNG',
         overview:'Simple Application build with <a href="https://www.accuweather.com">Accuweather</a> API.Used Tailwindcss for css.',
         url:'Blink'
     },
     {
         name:'Color Flipper',
-        img_path:'images/color_flipper.png',
+        img_path:'images/color_flipper.PNG',
         overview:'Simple Application build with <a href="https://www.accuweather.com">Accuweather</a> API.Used Tailwindcss for css.',
         url:'ColorFlipper'
     },
     {
         name:'Count Down Timer',
-        img_path:'images/count_down.png',
+        img_path:'images/count_down.PNG',
         overview:'Simple Application build with <a href="https://www.accuweather.com">Accuweather</a> API.Used Tailwindcss for css.',
         url:'CountDownTimer'
     },
     {
         name:'Counter',
-        img_path:'images/counter.png',
+        img_path:'images/counter.PNG',
         overview:'Simple Application build with <a href="https://www.accuweather.com">Accuweather</a> API.Used Tailwindcss for css.',
         url:'Counter'
     },
     {
         name:'Darkmode Toggle',
-        img_path:'images/dark_mode.png',
+        img_path:'images/dark_mode.PNG',
         overview:'Simple Application build with <a href="https://www.accuweather.com">Accuweather</a> API.Used Tailwindcss for css.',
         url:'DarkmodeToggle'
     },
     {
         name:'Notes',
-        img_path:'images/notes.png',
+        img_path:'images/notes.PNG',
         overview:'Simple Application build with <a href="https://www.accuweather.com">Accuweather</a> API.Used Tailwindcss for css.',
         url:'Notes'
     },
     {
         name:'Password Viewer',
-        img_path:'images/password_viewr.png',
+        img_path:'images/password_viewr.PNG',
         overview:'Simple Application build with <a href="https://www.accuweather.com">Accuweather</a> API.Used Tailwindcss for css.',
         url:'PasswordViewer'
     },
     {
         name:'Side Navigation',
-        img_path:'images/side_nav.png',
+        img_path:'images/side_nav.PNG',
         overview:'Simple Application build with <a href="https://www.accuweather.com">Accuweather</a> API.Used Tailwindcss for css.',
         url:'SideNav'
     },
     {
         name:'Toast',
-        img_path:'images/toast.png',
+        img_path:'images/toast.PNG',
         overview:'Simple Application build with <a href="https://www.accuweather.com">Accuweather</a> API.Used Tailwindcss for css.',
         url:'Toast'
     },
     {
         name:'Typing Effect',
-        img_path:'images/typing_effect.png',
+        img_path:'images/typing_effect.PNG',
         overview:'Simple Application build with <a href="https://www.accuweather.com">Accuweather</a> API.Used Tailwindcss for css.',
         url:'TypingEffect'
     },
